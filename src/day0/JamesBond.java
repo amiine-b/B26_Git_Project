e285e128fe24205d1f4554feaae5a5deb997ec4b
iamine <bergui.amine@gmail.com>
2022-04-15 12:35:09 -0400

    Created first file and added comments

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -4,7 +4,8 @@ public class JamesBond {
   
   public static void main(String [] args){
     
-    System.out.println("james Bond hacked the project")
+    System.out.println("james Bond hacked the project");
+
     }
 }
     
